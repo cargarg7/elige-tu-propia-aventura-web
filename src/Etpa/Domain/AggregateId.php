92c9e99745dfb90658641ec80cd5f2a4955d2c45
Carlos Buenosvinos <hi@carlos.io>
2014-06-13 01:11:31 +0200

    Using AggregateIdentifiers with Doctrine

--- src/Etpa/Domain/AggregateId.php
+++ src/Etpa/Domain/AggregateId.php
@@ -4,6 +4,10 @@ namespace Etpa\Domain;
 
 use Etpa\Infrastructure\Identity\UuidGenerator;
 
+/**
+ * Class AggregateId
+ * @package Etpa\Domain
+ */
 abstract class AggregateId
 {
     /**
@@ -11,16 +15,25 @@ abstract class AggregateId
      */
     protected $id;
 
+    /**
+     * @param string $id
+     */
     public function __construct($id = null)
     {
         $this->id = $id ?: UuidGenerator::generate();
     }
 
+    /**
+     * @return string
+     */
     public function id()
     {
         return $this->id;
     }
 
+    /**
+     * @return string
+     */
     public function __toString()
     {
         return $this->id();
